8a41e259e45d59ff459d901299ca7cf334103ea4
Paul Husek <paulhusek@hotmail.com>
2025-02-19 20:01:12 -0500

    Extracted onChange method

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,20 +1,23 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { MAX_CHARACTERS } from "../lib/constants.ts";
 
 export default function FeedbackForm() {
   const [text, setText] = useState("");
   const charCount = MAX_CHARACTERS - text.length;
 
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newTest = e.target.value;
+    console.log("inside handleChange");
+    if (newTest.length <= MAX_CHARACTERS) {
+      setText(e.target.value);
+    }
+  };
+
   return (
     <form className="form">
       <textarea
         value={text}
-        onChange={(e) => {
-          const newTest = e.target.value;
-          if (newTest.length <= MAX_CHARACTERS) {
-            setText(e.target.value);
-          }
-        }}
+        onChange={handleChange}
         id="feedback-textarea"
         placeholder="blablablabla"
         spellCheck={false}
